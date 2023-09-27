2208c6505bcb09ee10d6824c565d61a768f6499a
Not-yourCoder <84792472+Not-yourCoder@users.noreply.github.com>
2023-09-27 16:47:29 +0530

    Changed from Card to Table View

--- src/components/HeaderElement.jsx
+++ src/components/HeaderElement.jsx
@@ -15,8 +15,8 @@ import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 import MaterialUISwitch from "../components/muiSwitch";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Paper from '@mui/material/Paper';
-
+import Paper from "@mui/material/Paper";
+import TableData from "./TableData";
 
 const HeaderElement = () => {
   const pages = ["Products", "Pricing", "Blog"];
@@ -44,7 +44,7 @@ const HeaderElement = () => {
 
   const theme = createTheme({
     palette: {
-      mode: mode ? "dark" : "light"
+      mode: mode ? "dark" : "light",
     },
   });
 
@@ -54,141 +54,155 @@ const HeaderElement = () => {
     navigate("/login");
   };
   return (
-    <ThemeProvider theme = {theme}>
+    <ThemeProvider theme={theme}>
       <Paper>
-      <AppBar position="static">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-            <Typography
-              variant="h6"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "none", md: "flex" },
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-                
-              }}
-            >
-              LOGO
-            </Typography>
-
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
+        <AppBar position="static">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+              <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
                 sx={{
-                  display: { xs: "block", md: "none" },
+                  mr: 2,
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
                 }}
               >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-            <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
-              }}
-            >
-              LOGO
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-              {pages.map((page) => (
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: "white", display: "block" }}
-                >
-                  {page}
-                </Button>
-              ))}
-            </Box>
+                LOGO
+              </Typography>
 
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
                 </IconButton>
-              </Tooltip>
-              {/* <Switch {...label}  onClick={()=> setMode(!mode)}/> */}
-              <MaterialUISwitch sx={{ m: 1 }} onClick={() =>setMode(!mode)}/>
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "left",
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: "block", md: "none" },
+                  }}
+                >
+                  {pages.map((page) => (
+                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                      <Typography
+                        textAlign="center"
+                        onClick={() => {
+                          if (page === "Products") {
+                            console.log("This is products page.");
+                          }
+                        }}
+                      >
+                        {page}
+                      </Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+              <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+              <Typography
+                variant="h5"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                  mr: 2,
+                  display: { xs: "flex", md: "none" },
+                  flexGrow: 1,
+                  fontFamily: "monospace",
+                  fontWeight: 700,
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
                 }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
               >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography
-                      textAlign="center"
-                      onClick={() => {
-                        if (setting === "Logout") {
-                          logout();
-                        }
-                      }}
-                    >
-                      {setting}
-                    </Typography>
-                  </MenuItem>
+                LOGO
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                {pages.map((page) => (
+                  <Button
+                    key={page}
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: "white", display: "block" }}
+                  >
+                    {page}
+                  </Button>
                 ))}
-              </Menu>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
+              </Box>
+
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip>
+                {/* <Switch {...label}  onClick={()=> setMode(!mode)}/> */}
+                <MaterialUISwitch
+                  sx={{ m: 1 }}
+                  onClick={() => setMode(!mode)}
+                />
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography
+                        textAlign="center"
+                        onClick={() => {
+                          if (setting === "Logout") {
+                            logout();
+                          }
+                        }}
+                      >
+                        {setting}
+                      </Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
       </Paper>
     </ThemeProvider>
   );
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,20 +1,32 @@
 import React, { useEffect, useState } from "react";
 import HeaderElement from "../HeaderElement";
 import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import { Stack, Pagination, CssBaseline, Typography } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { styled } from "@mui/system";
+import {
+  TablePagination,
+  tablePaginationClasses as classes,
+} from "@mui/base/TablePagination";
 
 const Home = () => {
   const url = "https://dev-api.mrcorporate.in/v1/company?%24limit=10&%24skip=0";
   const [data, setData] = useState([]);
-  const [page, setPage] = useState(1);
-  
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   const accessToken = localStorage.getItem("token");
-
-  
 
   const fetchData = async () => {
     try {
@@ -36,73 +48,144 @@ const Home = () => {
       console.error("Error fetching data:", error);
     }
   };
-  const handlePageChange = (event, newPage) => {
-    setPage(newPage);
-  };
+
   useEffect(() => {
     fetchData();
   }, []);
-  
 
   return (
-    
+    <Root sx={{ maxWidth: "100%", width: 500 }}>
       <Box
         fontFamily={"rubik"}
         sx={{
           display: "flex",
           justifyContent: "center",
           flexDirection: "column",
-          // bgcolor:"#ffd740"
-          bgcolor:"#b2ebf2"
+          bgcolor: "#b2ebf2",
         }}
       >
         <HeaderElement />
-        {data.length > 0 && (
-          <div>
-            {data.slice(page * 1 - 1, page * 1).map((item) => (
-              <Card sx={{ maxWidth: 300, m: "10rem auto" }}>
-                <CardContent>
-                  <Typography
-                    sx={{ fontSize: 18 }}
-                    color="text.secondary"
-                    gutterBottom
-                  >
-                    {item.name}
-                  </Typography>
-
-                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    {!item.about ? (
-                      <Typography>There is no about us move on!</Typography>
-                    ) : (
-                      item.about
-                    )}
-                  </Typography>
-                  <Typography variant="body2" sx={{ fontSize: 15 }}>
-                    {!item.description ? (
-                      <Typography>
-                        We are currently writing description!
-                      </Typography>
-                    ) : (
-                      item.description
-                    )}
-                    <br />
-                    {item.email}
-                  </Typography>
-                </CardContent>
-              </Card>
-            ))}
-          </div>
-        )}
-        <Stack spacing={4} sx={{ m: "9rem auto" }}>
-          <Pagination
-            count={data.length}
-            onChange={handlePageChange}
-            variant="rounded"
-          />
-        </Stack>
+        <div style={{ maxWidth: "100%", width: 500 }}>
+          <table aria-label="custom pagination table">
+            <thead>
+              <tr>
+                <th>Company Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.length > 0 &&
+                data
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((item) => (
+                    <tr key={item.name}>
+                      <td>{item.name}</td>
+                      <td style={{ width: 160 }} align="right">
+                        {item.email}
+                      </td>
+                      <td style={{ width: 160 }} align="right">
+                        {item.phone}
+                      </td>
+                    </tr>
+                  ))}
+              {emptyRows > 0 && (
+                <tr style={{ height: 41 * emptyRows }}>
+                  <td colSpan={3} aria-hidden />
+                </tr>
+              )}
+            </tbody>
+            <tfoot>
+              <tr>
+                <CustomTablePagination
+                  rowsPerPageOptions={[5, 10, { label: "All", value: -1 }]}
+                  colSpan={3}
+                  count={data.length}
+                  rowsPerPage={rowsPerPage}
+                  page={page}
+                  slotProps={{
+                    select: {
+                      "aria-label": "rows per page",
+                    },
+                    actions: {
+                      showFirstButton: true,
+                      showLastButton: true,
+                    },
+                  }}
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+              </tr>
+            </tfoot>
+          </table>
+        </div>
       </Box>
-   
+    </Root>
   );
 };
 
 export default Home;
+
+/* STYLED */
+const CustomTablePagination = styled(TablePagination)`
+  & .${classes.toolbar} {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+
+    @media (min-width: 768px) {
+      flex-direction: row;
+      align-items: center;
+    }
+  }
+
+  & .${classes.selectLabel} {
+    margin: 0;
+  }
+
+  & .${classes.displayedRows} {
+    margin: 0;
+
+    @media (min-width: 768px) {
+      margin-left: auto;
+    }
+  }
+
+  & .${classes.spacer} {
+    display: none;
+  }
+
+  & .${classes.actions} {
+    display: flex;
+    gap: 0.25rem;
+  }
+`;
+
+const Root = styled("div")(
+  ({ theme }) => `
+  table {
+    font-family: IBM Plex Sans, sans-serif;
+    font-size: 0.875rem;
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  td,
+  th {
+    border: 1px solid ${theme.palette.mode === "dark" ? grey[800] : grey[200]};
+    text-align: left;
+    padding: 8px;
+  }
+
+  th {
+    background-color: ${theme.palette.mode === "dark" ? grey[900] : "#fff"};
+  }
+  `
+);
+const grey = {
+  200: '#d0d7de',
+  800: '#32383f',
+  900: '#24292f',
+};
+
